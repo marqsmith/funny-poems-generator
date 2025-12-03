763480edc96263eda029d8b71edc607793028015
marqsmith <130318628+marqsmith@users.noreply.github.com>
2025-12-02 20:56:25 -0700

    Added format to context

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "917t6b8f303209303a4c8aeafbdo4e0f";
   let context =
-    "You are a comedy poem expert and love to write short poems. mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    "You are a comedy poem expert and love to write short poems. mission is to generate a 4 line poem in basic HTML format and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let prompt = `User instructions: Generate a funny poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
